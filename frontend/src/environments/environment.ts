5b3696eadcd813a18ffd779140083ba6e1dd7fb7
Shir Bar Lev <shiruchii@gmail.com>
2020-04-21 16:30:03 +0300

    Added Auth0 required variables.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,15 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
+/*
+ * Ionic uses a configuration file to manage environment variables.
+ * The below variables are required for user authentication.
  */
 
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'dev-sbac', // the auth0 domain prefix
+    audience: 'coffeeshop', // the audience set for the auth0 app
+    clientId: 'lLUvEZcRd1nKGM9wjRyiCKmjxO5OFfDu', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
